f76ea6dbef94616b2ab6b4df6f949bf14015142b
Rushvi Patel <umm9ef@virginia.edu>
2022-09-06 20:09:23 +0000

    1

--- src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_validity_z3.py
+++ src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_validity_z3.py
@@ -2,12 +2,13 @@ from z3 import *
 
 def isValid(P) :
     s = Solver()
-    s.add(True) # replace True with required declarative spec
+    s.add(Not(P)) # replace True with required declarative spec
     return (s.check()==unsat)
+# checks if not(x or not x) is unsat
 
 # Declare X to be a Z3 Bool variable
 X = Bool('X')
 # Print the result of testing (X Or Not X) for validity
-print(isValid(Or(X, Not(X))))
+print(isValid(Or(X, Not(X)))) # true because x is valid
 # Print the result of testing (X And Not X) for validity
-print(isValid(And(X, Not(X))))
+print(isValid(And(X, Not(X)))) # false because x is not valid
