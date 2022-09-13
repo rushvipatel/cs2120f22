b606d39989dab7d9b07e5056e1bf96d2c5ead01c
Rushvi Patel <72409300+rushvipatel@users.noreply.github.com>
2022-09-13 22:09:51 +0000

    .

--- src/mywork/hw2.py
+++ src/mywork/hw2.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-from z3 import *
-
-
-
-=======
 # Be sure you've done pip install z3-solver
 from telnetlib import X3PAD
 from z3 import *
@@ -13,48 +7,68 @@ from z3 import *
 # point on a working program to solve some problem
 # of interest. Here the problem is to compute and
 # return a non-negative square root of argument, n 
->>>>>>> 5779e4baff43c1917c72c67c9391c88923ed1090
 def hw2():
     
     
     # Create z3 variable(s) representing the unknown
-<<<<<<< HEAD
-=======
     # Here, the unknown, x, is the square root of n.
->>>>>>> 5779e4baff43c1917c72c67c9391c88923ed1090
     X, Y, Z = Bools('X Y Z')
     
     s = Solver()
     
-<<<<<<< HEAD
-    # X ∨ Y, X ⊢ ¬Y 
-    # in propositional logic "or" is not exclusive
-    
-    
-    C1 = Implies(And(Or(X,Y), X), Not(Y))
-    s.add(Not(C1))
-    # I believe it's not valid because if x is true doesnt mean that y is false because or doesnt mean 'only' one or the other
-    
-    # Run the Z3 model finder, capturing "sat"
-    # or "unsat" as the return value 
-    r = s.check()
-
-
-    
-    if (r == unsat) :
-        print("C1 is valid")
-    else : #counter example: look for model of not c1
-        print("Heres a counter example: ", s.model) 
-
-# 2. X, Y ⊢ X ∧ Y              -- and introductio
-# n  x ^ y  x ^ y if we know x and y is true then we know they are both true
-=======
     # 1. X ∨ Y, X ⊢ ¬Y 
     # As proposition in PL: ((X \/ Y) /\ X) -> ~Y
     C1 = Implies(And(Or(X,Y),X),Not(Y))
     
     s.add(Not(C1))
+    s.reset()
     # I believe it's not valid
+    
+    # 2. X, Y ⊢ X ∧ Y              -- and introduction
+    C2 = Implies(And(X,Y),And(X,Y))
+    s.add(Not(C1))
+    s.reset()
+
+    # 3. X ∧ Y ⊢ X                 -- and elimination left
+    C3 = Implies(And(X,Y),X)
+    s.add(Not(C3))
+    s.reset()
+    
+    # 4. X ∧ Y ⊢ Y                 -- and elimination right
+    C4 = Implies(And(X,Y),Y)
+    s.add(Not(C4))
+    s.reset()
+    # 5. ¬¬X ⊢ X                   -- negation elimination 
+    C5 = Implies(Not(Not(X)),X)
+    s.add(Not(C5))
+    s.reset()
+    # 6. ¬(X ∧ ¬X)                 -- no contradiction
+    C6 = Not(And(X,Not(X)))
+    s.add(Not(C6))
+    s.reset()
+    # 7. X ⊢ X ∨ Y                 -- or introduction left
+    C7 = Implies(X,Or(X,Y))
+    s.add(Not(C7))
+    s.reset()
+    # 8. Y ⊢ X ∨ Y                 -- or introduction right
+    C8 = Implies(y,Or(X,Y))
+    s.add(Not(C8))
+    s.reset()
+    # 9. X → Y, ¬X ⊢ ¬ Y           -- denying the antecedent
+    C9 = Implies(X,Or(X,Y))
+    s.add(Not(C9))
+    s.reset()
+    # 10. X → Y, Y → X ⊢ X ↔ Y      -- iff introduction
+    # 11. X ↔ Y ⊢ X → Y            -- iff elimination left
+    # 12. X ↔ Y ⊢ Y → X            -- iff elimination right
+    # 13. X ∨ Y, X → Z, Y → Z ⊢ Z  -- or elimination
+    # 14. X → Y, Y ⊢ X             -- affirming the conclusion
+    # 15. X → Y, X ⊢ Y             -- arrow elimination
+    # 16. X → Y, Y → Z ⊢ X → Z     -- transitivity of → 
+    # 17. X → Y ⊢ Y → X            -- converse
+    # 18. X → Y ⊢ ¬Y → ¬X          -- contrapositive
+    # 19. ¬(X ∨ Y) ↔ ¬X ∧ ¬Y       -- DeMorgan #1 (¬ distributes over ∨)
+    # 20. ¬(X ∧ Y) ↔ ¬X ∨ ¬Y       -- Demorgan #2 (¬ distributes over ∧)
   
     r = s.check()
     
@@ -66,5 +80,4 @@ def hw2():
         print("Here's a counter-example: ", s.model() )
 
 
->>>>>>> 5779e4baff43c1917c72c67c9391c88923ed1090
 hw2()
